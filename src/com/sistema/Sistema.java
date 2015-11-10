53c60b2b73c0ee33c28844ab8af08ca293922354
Suzane <Suzane@asus-ssd>
2015-11-10 10:13:06 -0300

    tentando denovo

--- src/com/sistema/Sistema.java
+++ src/com/sistema/Sistema.java
@@ -5,6 +5,7 @@ public class Sistema {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo");
 		System.out.println("Hola denovo");
+		System.out.println("tentando de novo");
 
 	}
 
